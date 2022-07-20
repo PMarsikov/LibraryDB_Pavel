2bb2842c2f75b7998b73c309a9673c718d977c74
Pavel Marsikau <PMarsikau@gomel.iba.by>
2022-07-20 08:54:45 +0300

    export to excel logic

--- LibraryDB_Pavel/LibraryDB_Pavel/ViewModel/BooksViewModel.cs
+++ LibraryDB_Pavel/LibraryDB_Pavel/ViewModel/BooksViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
@@ -13,6 +14,10 @@ using LibraryDB_Pavel.Repository.Interfaces;
 using LibraryDB_Pavel.Utils.Constants;
 using LibraryDB_Pavel.Utils.Enums;
 using Microsoft.EntityFrameworkCore;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data;
+using System.Data.OleDb;
+using System.Reflection;
 
 namespace LibraryDB_Pavel.ViewModel
 {
@@ -79,7 +84,7 @@ namespace LibraryDB_Pavel.ViewModel
                                         var ppp = book;
                                         Books.Add(book);
                                         _bookRepository.Create(book);
-                                        
+
                                     }
                                 }
                                 catch (Exception e)
@@ -123,7 +128,7 @@ namespace LibraryDB_Pavel.ViewModel
                         xdoc.Add(root);
                         var path = FilePath(BookConstants.FileNameXml, ".xml");
                         xdoc.Save(path);
-                        MessageBox.Show(string.Format(MessagesConstants.DataExportedToXml, path));
+                        MessageBox.Show(string.Format(MessagesConstants.DataExportedToFile, path));
                     }));
             }
         }
@@ -136,9 +141,75 @@ namespace LibraryDB_Pavel.ViewModel
                 return //_openCommand ??
                     (_exportToXmlCommand = new RelayCommand(obj =>
                     {
-                        
-                        MessageBox.Show("Exporting to excel . . .");
+                        var path = FilePath(BookConstants.FileNameExcel, ".xlsx");
+                        var data = ToDataTable(Books.ToList());
+                        ToExcelFile(data, path);
+                        MessageBox.Show(string.Format(MessagesConstants.DataExportedToFile, path));
                     }));
+            }
+        }
+
+        public static DataTable ToDataTable<T>(List<T> items)
+        {
+            var dataTable = new DataTable(typeof(T).Name);
+
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var prop in properties)
+            {
+                var type = (prop.PropertyType.IsGenericType &&
+                            prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>)
+                    ? Nullable.GetUnderlyingType(prop.PropertyType)
+                    : prop.PropertyType);
+                dataTable.Columns.Add(prop.Name, type);
+            }
+
+            foreach (var item in items)
+            {
+                var values = new object[properties.Length];
+                for (var i = 0; i < properties.Length; i++)
+                {
+                    values[i] = properties[i].GetValue(item, null);
+                }
+
+                dataTable.Rows.Add(values);
+            }
+
+            return dataTable;
+        }
+
+        public static void ToExcelFile(DataTable dataTable, string filePath, bool overwriteFile = true)
+        {
+            if (File.Exists(filePath) && overwriteFile)
+                File.Delete(filePath);
+
+            using (var connection = new OleDbConnection())
+            {
+                connection.ConnectionString = string.Format(BookConstants.ExcelPathConstant, filePath);
+                connection.Open();
+                using (var command = new OleDbCommand())
+                {
+                    command.Connection = connection;
+                    var columnNames = (from DataColumn dataColumn in dataTable.Columns select dataColumn.ColumnName)
+                        .ToList();
+                    var tableName = !string.IsNullOrWhiteSpace(dataTable.TableName)
+                        ? dataTable.TableName
+                        : Guid.NewGuid().ToString();
+                    command.CommandText =
+                        $"CREATE TABLE [{tableName}] ({string.Join(",", columnNames.Select(c => $"[{c}] VARCHAR").ToArray())});";
+                    command.ExecuteNonQuery();
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        var rowValues = (from DataColumn column in dataTable.Columns
+                            select (row[column] != null && row[column] != DBNull.Value)
+                                ? row[column].ToString()
+                                : string.Empty).ToList();
+                        command.CommandText =
+                            $"INSERT INTO [{tableName}]({string.Join(",", columnNames.Select(c => $"[{c}]"))}) VALUES ({string.Join(",", rowValues.Select(r => $"'{r}'").ToArray())});";
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                connection.Close();
             }
         }
 
@@ -157,6 +228,7 @@ namespace LibraryDB_Pavel.ViewModel
                                 MessageBox.Show(MessagesConstants.ErrorInputMsg);
                                 return;
                             }
+
                             var books = _dbContext.Books;
                             var filteredBooks = FilteredList(_filterPropertyName, books);
                             Books.Clear();
