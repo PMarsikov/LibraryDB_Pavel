8045d8bf59ae18cc20c162e42e655b433ec0b6f1
Pavel Marsikau <PMarsikau@gomel.iba.by>
2022-07-20 09:04:47 +0300

    excel,xml moved to helper

--- LibraryDB_Pavel/LibraryDB_Pavel/ViewModel/BooksViewModel.cs
+++ LibraryDB_Pavel/LibraryDB_Pavel/ViewModel/BooksViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
@@ -14,10 +13,7 @@ using LibraryDB_Pavel.Repository.Interfaces;
 using LibraryDB_Pavel.Utils.Constants;
 using LibraryDB_Pavel.Utils.Enums;
 using Microsoft.EntityFrameworkCore;
-using Excel = Microsoft.Office.Interop.Excel;
-using System.Data;
-using System.Data.OleDb;
-using System.Reflection;
+using LibraryDB_Pavel.Helpers;
 
 namespace LibraryDB_Pavel.ViewModel
 {
@@ -110,24 +106,8 @@ namespace LibraryDB_Pavel.ViewModel
                 return //_openCommand ??
                     (_exportToXmlCommand = new RelayCommand(obj =>
                     {
-                        XDocument xdoc = new XDocument();
-                        XElement root = new XElement("TestProgram");
-                        foreach (var book in Books)
-                        {
-                            XElement record = new XElement("Record");
-                            record.Add(new XAttribute("id", book.Id));
-                            record.Add(new XElement("FirstName", book.AuthorFirstName));
-                            record.Add(new XElement("MiddleName", book.AuthorMiddleName));
-                            record.Add(new XElement("AuthorLastName", book.AuthorLastName));
-                            record.Add(new XElement("AuthorBirthDay", book.AuthorBirthDay));
-                            record.Add(new XElement("BookTitle", book.BookTitle));
-                            record.Add(new XElement("BookYear", book.BookYear));
-                            root.Add(record);
-                        }
-
-                        xdoc.Add(root);
                         var path = FilePath(BookConstants.FileNameXml, ".xml");
-                        xdoc.Save(path);
+                        ExportToXml.SaveToXml(Books, path);
                         MessageBox.Show(string.Format(MessagesConstants.DataExportedToFile, path));
                     }));
             }
@@ -142,74 +122,10 @@ namespace LibraryDB_Pavel.ViewModel
                     (_exportToXmlCommand = new RelayCommand(obj =>
                     {
                         var path = FilePath(BookConstants.FileNameExcel, ".xlsx");
-                        var data = ToDataTable(Books.ToList());
-                        ToExcelFile(data, path);
+                        var data = ExportDataToExcel.ToDataTable(Books.ToList());
+                        ExportDataToExcel.ToExcelFile(data, path);
                         MessageBox.Show(string.Format(MessagesConstants.DataExportedToFile, path));
                     }));
-            }
-        }
-
-        public static DataTable ToDataTable<T>(List<T> items)
-        {
-            var dataTable = new DataTable(typeof(T).Name);
-
-            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var prop in properties)
-            {
-                var type = (prop.PropertyType.IsGenericType &&
-                            prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>)
-                    ? Nullable.GetUnderlyingType(prop.PropertyType)
-                    : prop.PropertyType);
-                dataTable.Columns.Add(prop.Name, type);
-            }
-
-            foreach (var item in items)
-            {
-                var values = new object[properties.Length];
-                for (var i = 0; i < properties.Length; i++)
-                {
-                    values[i] = properties[i].GetValue(item, null);
-                }
-
-                dataTable.Rows.Add(values);
-            }
-
-            return dataTable;
-        }
-
-        public static void ToExcelFile(DataTable dataTable, string filePath, bool overwriteFile = true)
-        {
-            if (File.Exists(filePath) && overwriteFile)
-                File.Delete(filePath);
-
-            using (var connection = new OleDbConnection())
-            {
-                connection.ConnectionString = string.Format(BookConstants.ExcelPathConstant, filePath);
-                connection.Open();
-                using (var command = new OleDbCommand())
-                {
-                    command.Connection = connection;
-                    var columnNames = (from DataColumn dataColumn in dataTable.Columns select dataColumn.ColumnName)
-                        .ToList();
-                    var tableName = !string.IsNullOrWhiteSpace(dataTable.TableName)
-                        ? dataTable.TableName
-                        : Guid.NewGuid().ToString();
-                    command.CommandText =
-                        $"CREATE TABLE [{tableName}] ({string.Join(",", columnNames.Select(c => $"[{c}] VARCHAR").ToArray())});";
-                    command.ExecuteNonQuery();
-                    foreach (DataRow row in dataTable.Rows)
-                    {
-                        var rowValues = (from DataColumn column in dataTable.Columns
-                            select (row[column] != null && row[column] != DBNull.Value)
-                                ? row[column].ToString()
-                                : string.Empty).ToList();
-                        command.CommandText =
-                            $"INSERT INTO [{tableName}]({string.Join(",", columnNames.Select(c => $"[{c}]"))}) VALUES ({string.Join(",", rowValues.Select(r => $"'{r}'").ToArray())});";
-                        command.ExecuteNonQuery();
-                    }
-                }
-
-                connection.Close();
             }
         }
 
